6be527ea2c83530eab993a0c400db1c274b98099
Malcolm Nixon <Malcolm.nixon@gmail.com>
2022-07-15 23:46:51 -0400

    Updated to godot-xr-tools 2.5.0 RC

--- addons/godot-xr-tools/assets/HandPhysicsBone.gd
+++ addons/godot-xr-tools/assets/HandPhysicsBone.gd
@@ -10,11 +10,11 @@ extends BoneAttachment
 ##
 ##     It extends from BoneAttachment to track the position of the bone in
 ##     the hand skeleton, and uses this position to move a CapsuleShape bone
-##     StaticBody.
+##     KinematicBody.
 ##
-##     The bone StaticBody is set as top-level and manually driven with the 
+##     The bone Kinematic is set as top-level and manually driven with the 
 ##     bones positon and rotation. This is to prevent hand-scaling from scaling
-##     the StaticBody collision shape as colliders cannot tolerate being scaled.
+##     the Kinematic collision shape, as colliders cannot tolerate being scaled.
 ##
 ##     To handle scaling, this script subscribes to the hand_scale_changed signal
 ##     emitted by the XRToolsHand script and manually adjusts the CapsuleShape
@@ -41,11 +41,11 @@ export var bone_group := ""
 # Bone shape
 var _bone_shape : CapsuleShape
 
-# Bone body
-var _bone_body : StaticBody
+# Physics-bone body
+var _physics_bone : KinematicBody
 
-# Bone middle
-var _bone_middle : Spatial
+# Skeletal-bone spatial
+var _skeletal_bone : Spatial
 
 
 # Called when the node enters the scene tree for the first time.
@@ -67,36 +67,60 @@ func _ready():
 	bone_collision.transform.basis = Basis(Vector3.RIGHT, PI/2)
 
 	# Construct the bone body
-	_bone_body = StaticBody.new()
-	_bone_body.set_name("BoneBody")
-	_bone_body.set_as_toplevel(true)
-	_bone_body.collision_layer = physics_hand.collision_layer | collision_layer
-	_bone_body.add_child(bone_collision)
+	_physics_bone = KinematicBody.new()
+	_physics_bone.set_name("BoneBody")
+	_physics_bone.set_as_toplevel(true)
+	_physics_bone.collision_layer = physics_hand.collision_layer | collision_layer
+	_physics_bone.collision_mask = 0
+	_physics_bone.add_child(bone_collision)
 
 	# Set the optional bone group for all bones in the hand
 	if not physics_hand.bone_group.empty():
-		_bone_body.add_to_group(physics_hand.bone_group)
+		_physics_bone.add_to_group(physics_hand.bone_group)
 
 	# Set the optional bone group for this one bone
 	if not bone_group.empty():
-		_bone_body.add_to_group(bone_group)
+		_physics_bone.add_to_group(bone_group)
 
 	# Construct the bone middle spatial
-	_bone_middle = Spatial.new()
-	_bone_middle.transform.origin = Vector3.UP * length / 2
+	_skeletal_bone = Spatial.new()
+	_skeletal_bone.transform.origin = Vector3.UP * length / 2
 
 	# Add the bone body to this hand bone
-	add_child(_bone_body)
-	add_child(_bone_middle)
+	add_child(_physics_bone)
+	add_child(_skeletal_bone)
+
+	# Perform initial teleport of the bone
+	_teleport_bone()
 
 
 # Handle bone updating in the physics process
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	_move_bone(delta)
+
+
+# Moves the physics-bone to the skeletal-bone
+func _move_bone(delta: float) -> void:
 	# Get the bone transform
-	var bone_xform = _bone_middle.global_transform
+	var bone_xform := _skeletal_bone.global_transform
+
+	# Get the required velocity
+	var bone_vel := (bone_xform.origin - _physics_bone.global_transform.origin) / delta
+
+	# Move the bone into position
+	_physics_bone.move_and_slide(bone_vel, Vector3.UP)
+
+	# Rotate the bone into the correct rotation
+	_physics_bone.global_transform.basis = bone_xform.basis
+
+
+# Teleports the physics-bone to the skeletal-bone
+func _teleport_bone() -> void:
+	# Get the bone transform
+	var bone_xform := _skeletal_bone.global_transform
 
 	# Set the bone position
-	_bone_body.global_transform = Transform(
+	_physics_bone.global_transform = Transform(
 		Basis(bone_xform.basis.get_rotation_quat()),
 		bone_xform.origin)
 
